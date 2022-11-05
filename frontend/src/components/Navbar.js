695ca4c18614c6bad273a61cf521151932b127c9
Jakub Klewicki <kuba.klewicki@gmail.com>
2022-11-05 21:47:47 +0100

    Change media querry in Navbar to md:

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
     return ( 
             <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white ">
                 <Link to='/'><span className='flex'><h1 className='w-full text-3xl font-bold text-[#00df9a]'>Toolman</h1><AiOutlineTool size={30} color='#00df9a'/></span></Link>
-                <ul className='hidden lg:flex'>
+                <ul className='hidden md:flex'>
                     <Link to='/'><li className='p-4'>Home</li></Link>
                     <li className='p-4'>Items</li>
                     <li className='p-4'>Workers</li>
@@ -22,11 +22,11 @@ const Navbar = () => {
                 </ul>
                 
                 {/* onClick change icon of menu depend on the state */}
-                <div onClick={handleNav} className='block lg:hidden' >
+                <div onClick={handleNav} className='block md:hidden' >
                     {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
                 </div>
                 {/* changes styling depend on the nav state */}
-                <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 lg:hidden' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+                <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 md:hidden' : 'ease-in-out duration-500 fixed left-[-100%]'}>
                     <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Toolman</h1>
                         <li className='p-4 border-b border-gray-700'>Home</li>
                         <li className='p-4 border-b border-gray-700'>Items</li>
