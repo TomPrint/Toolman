016b98a6b55c5a81cf42d4b73709b4ffa57a8be2
Jakub Klewicki <kuba.klewicki@gmail.com>
2022-11-30 23:41:58 +0100

    styling modal and SingleItem

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -2,27 +2,16 @@ import React from "react";
 
 function Modal(props) {
   return (
-    <div className="bg-[#808080]">
-      <div className="text-white">
-        <div className="titleCloseBtn">
-          <button
-            onClick={() => {
+        <div class="bg-slate-700 bg-opacity-50 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0">
+          <div class="bg-[#ffffff] px-10 py-14 rounded-md text-center">
+            <h1 class="text-xl mb-4 font-bold text-slate-500">{`Czy chcesz na pewno usunąć ${props.description}?`}</h1>
+            <button class="bg-[#656565] px-7 py-2 rounded-md text-md text-white font-semibold" onClick={() => {
               props.setOpenModal(false);
-            console.log("kliknięto w zamknięcie")
-            }}
-          >
-            X
-          </button>
+            }}>Nie</button>
+            <button class="bg-red-500 px-7 py-2 ml-2 rounded-md text-md text-white font-semibold" onClick={props.handleDelete}>Tak</button>
+          </div>
         </div>
-        <div className="title">
-          <h1>Czy na pewno chcesz usunąć to narzędzie ?</h1>
-        </div>
-     
-          <button onClick={props.handleDelete}>Tak</button>
-
-        </div>
-      </div>
-  );
+        )
 }
 
 export default Modal;
